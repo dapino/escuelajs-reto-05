58e13171789aaf1be065f9e6988b82347c0d246e
Daniel <hola@dapino.co>
2019-09-28 10:42:16 -0500

    feat: save the next_fetch on localStorage

--- src/index.js
+++ src/index.js
@@ -3,36 +3,47 @@ const $observe = document.getElementById('observe');
 const API = 'https://rickandmortyapi.com/api/character/';
 
 const getData = api => {
-  fetch(api)
-    .then(response => response.json())
-    .then(response => {
-      const characters = response.results;
-      let output = characters.map(character => {
-        return `
-      <article class="Card">
-        <img src="${character.image}" />
-        <h2>${character.name}<span>${character.species}</span></h2>
-      </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
-      newItem.innerHTML = output;
-      $app.appendChild(newItem);
-    })
-    .catch(error => console.log(error));
-}
+    const next_fetch = localStorage.getItem('next_fetch');
+    let API_URL = api;
+
+    if (!next_fetch) {
+        console.log('next_fetch?, no, doesnt exist that.' )
+    } else {
+      console.log('Yes!, next_fetch exist!')
+      console.log(next_fetch)
+    }
+
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(response => {
+            const characters = response.results;
+            localStorage.setItem('next_fetch', response.info.next);
+            let output = characters.map(character => {
+                return `
+                  <article class="Card">
+                    <img src="${character.image}" />
+                    <h2>${character.name}<span>${character.species}</span></h2>
+                  </article>
+                `
+            }).join('');
+            let newItem = document.createElement('section');
+            newItem.classList.add('Items');
+            newItem.innerHTML = output;
+            $app.appendChild(newItem);
+        })
+        .catch(error => console.log(error));
+};
 
 const loadData = () => {
-  getData(API);
-}
+    getData(API);
+};
 
 const intersectionObserver = new IntersectionObserver(entries => {
-  if (entries[0].isIntersecting) {
-    loadData();
-  }
+    if (entries[0].isIntersecting) {
+        loadData();
+    }
 }, {
-  rootMargin: '0px 0px 100% 0px',
+    rootMargin: '0px 0px 100% 0px',
 });
 
 intersectionObserver.observe($observe);
